caa16f2ed3a86db34dcb2728057f31d7faffb71b
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2015-09-14 11:50:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+flycheck-mypy
+=================
+
+[![MELPA](http://melpa.org/packages/flycheck-mypy-badge.svg)](http://melpa.org/#/flycheck-mypy)
+
+Since version [3.5](https://www.python.org/downloads/release/python-350/), Python supports typing hints.
+[MyPy](http://mypy-lang.org/) makes use of those hints to type-check your code.
+
+This package is a FlyCheck interface to MyPy.
---
+++ flycheck-mypy.el
@@ -0,0 +1,61 @@
+;;; flycheck-mypy.el --- Support mypy in flycheck
+
+;; Copyright (C) 2015 Lorenzo Bolla <lbolla@gmail.com>
+;;
+;; Author: Lorenzo Bolla <lbolla@gmail.com>
+;; Created: 14 Septermber 2015
+;; Version: 1.0
+;; Package-Requires: ((flycheck "0.18"))
+
+;;; Commentary:
+
+;; This package adds support for mypy to flycheck. To use it, add
+;; to your init.el:
+
+;; (require 'flycheck-mypy)
+;; (add-hook 'python-mode-hook 'flycheck-mode)
+
+;; If you want to use mypy you probably don't want pylint or
+;; flake8. To disable those checkers, add the following to your
+;; init.el:
+
+;; (add-to-list 'flycheck-disabled-checkers 'python-flake8)
+;; (add-to-list 'flycheck-disabled-checkers 'python-pylint)
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(require 'flycheck)
+
+(flycheck-define-checker python-mypy
+  "Mypy syntax checker. Requires mypy>=0.2.0.
+
+See URL `http://mypy-lang.org/'."
+
+  :command ("mypy" source-original)
+  :error-patterns
+  ((error line-start (file-name) ":" line ": error:" (message) line-end))
+  :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-mypy)
+
+(provide 'flycheck-mypy)
+;;; flycheck-mypy.el ends here
